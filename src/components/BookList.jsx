09c02fe67e619797df9017a4212e4b9d2ab961dc
Diana <dianadorojuc3@gmail.com>
2024-08-21 19:46:32 +0200

    last commit

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,12 @@
 import { Component } from "react";
-import { Container, Row, Form} from "react-bootstrap";
+import { Container, Row, Form, Col} from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
 class BookList extends Component {
+  // BookList nasce per ricevere dalle props un ARRAY di libri!
+  // chiameremo questa prop... booksArray
+
+// le props nei componenti a classe si recuperano dentro "this.props"
 
     state = {
         searchBook: '',
@@ -14,30 +18,32 @@ class BookList extends Component {
     }
 
     //filtro i libri 
-    filterBooks = () =>{
+    filtraLibri = () =>{
         const {searchBook} = this.state
         const {libri} =  this.props
 
         return libri.filter((book) =>
-        book.title.includes(searchBook))
+        book.title.toLowerCase().includes(searchBook.toLowerCase()))
 
     }
 
   render() {
-    const filteredBook = this.filterBooks()
+    const filteredBook = this.filtraLibri()
     return (
       <Container>
-        <Row className="my-3 justify-content-center">
+        <Row>
+            <Col className="my-3 d-flex justify-content-center">
             <Form.Control
             type="text" placeholder = "Cerca titolo libro"
             className="w-75"
             value = {this.state.searchBook}
             onChange = {this.handleSearch}>
             </Form.Control>
+            </Col>
             </Row>
            <Row className="gy-4">
-          {filteredBook.map((book) => (
-            <SingleBook   key={book.asin} libro={book} />
+          {filteredBook.map((scifiBook) => (
+            <SingleBook   key={scifiBook.asin} libro={scifiBook} />
          
           ))}
         </Row>
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,48 +1,53 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Col, Card, Button } from "react-bootstrap";
 
 class SingleBook extends Component {
-    state = {
-        selected: false,
-    };
+  state = {
+    selected: false,
+  };
 
-    toggleSelected = () => {
-        this.setState((prevState) => ({   //prevState sta per stato precedente. 
-            //l'oggetto di stato corrente del componente
-            selected: !prevState.selected, //Se selected è true !prevState.selected diventa false e viceversa. 
-        }));
-    };
+ // SingleBook deve mostrare UN solo libro
+  // si presuppone che questo libro gli arrivi dalle props
 
-// SingleBook deve mostrare UN solo libro
-// si presuppone che questo libro gli arrivi dalle props
+  // immaginiamo di invocare sempre SingleBook con una prop che si chiama "libro"
+  // ci possiamo aspettare lèoggetto del libro in this.props.libro
 
-// immaginiamo di invocare sempre SingleBook con una prop che si chiama "libro"
-// ci possiamo aspettare lèoggetto del libro in this.props.libro
-
-render() {
-    return(
-    
-                        <Col xs={12} md ={6} lg={3} key={this.props.libro.asin}>
-                        <Card
-                            className={`mb-4 h-100 ${this.state.selected ? 'border border-danger border-3' : ''}`}
-                            onClick={this.toggleSelected}
-                            style={{ cursor: "pointer" }} 
-                        >
-                        <Card.Img variant="top" className="card-img-top" src={this.props.libro.img} />
-                        <Card.Body className="pb-0">
-                          <Card.Title className="title-overflow">{this.props.libro.title}</Card.Title>
-                          <div className="d-flex justify-content-center">
-                          <Button variant="info" className="text-center px-3 text-light mb-3">
-                           {this.props.libro.price} $
-                          </Button>
-                          </div>
-                        </Card.Body>
-                      </Card>
-                      </Col>
-     
-         
-       
-    )
+  render() {
+    return (
+      <Col xs={12} md={6} lg={3} key={this.props.libro.asin}>
+        <Card
+          className={`mb-4 h-100 ${
+            this.state.selected ? "border border-danger border-3" : ""
+          }`}
+          //al onClick cambia il bordo
+          onClick={() => {
+            this.setState({
+                selected: !this.state.selected  //sempre
+            })
+          }}
+          style={{ cursor: "pointer" }}
+        >
+          <Card.Img
+            variant="top"
+            className="card-img-top"
+            src={this.props.libro.img}
+          />
+          <Card.Body className="pb-0">
+            <Card.Title className="title-overflow">
+              {this.props.libro.title}
+            </Card.Title>
+            <div className="d-flex justify-content-center">
+              <Button
+                variant="info"
+                className="text-center px-3 text-light mb-3"
+              >
+                {this.props.libro.price} $
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
 }
-}
-export default SingleBook
+export default SingleBook;
